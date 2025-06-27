76dcc4c262dfbf261a896e271e36b64b212da591
alikatecunningham <alisonkatecunningham@gmail.com>
2025-06-26 17:32:04 -0700

    update import paths

--- examples/run_app.py
+++ examples/run_app.py
@@ -1,6 +1,6 @@
 from src.scraping.driver.connect import define_driver
-from strongs_scraper.src.scraping.strongs_id_scraper import StrongsIDScraper
-from strongs_scraper.src.scraping.verse_scraper import VerseScraper
+from src.scraping.strongs_id_scraper import StrongsIDScraper
+from src.scraping.verse_scraper import VerseScraper
 from pathlib import Path
 import pandas as pd
 import os
@@ -8,19 +8,19 @@ import os
 driver = define_driver()
 
 # Build lookups for Strong's ID terms 
-strongs_id_scraper = StrongsIDScraper(driver=driver)
-strongs_id_scraper.run()
+# strongs_id_scraper = StrongsIDScraper(driver=driver)
+# strongs_id_scraper.run()
 
 # Define bible versions to iterate over
-# versions = pd.read_csv(Path.joinpath(Path(__file__).resolve().parents[1], 'versions', 'versions.csv'))
-# versions = versions['versions'].tolist()
-# versions = versions[1:]
+versions = pd.read_csv(Path.joinpath(Path(__file__).resolve().parents[1], 'versions', 'versions.csv'))
+versions = versions['versions'].tolist()
+versions = versions[2:]
 
-# search_term_dir = Path.joinpath(Path(__file__).resolve().parents[1], 'documents')
-# # Convert search_terms / versions to lists
-# for file in os.listdir(search_term_dir):
-# 	if 'csv' in file:
-# 		search_terms = pd.read_csv(os.path.join(search_term_dir,file))
-# 		search_terms = search_terms['search_terms'].tolist()
-# 		verse_scraper = VerseScraper(driver=driver,search_terms=search_terms,versions=versions)
-# 		verse_scraper.run()
+search_term_dir = Path.joinpath(Path(__file__).resolve().parents[1], 'documents')
+# Convert search_terms / versions to lists
+for file in os.listdir(search_term_dir):
+	if 'csv' in file:
+		search_terms = pd.read_csv(os.path.join(search_term_dir,file))
+		search_terms = search_terms['search_terms'].tolist()
+		verse_scraper = VerseScraper(driver=driver,search_terms=search_terms,versions=versions)
+		verse_scraper.run()
